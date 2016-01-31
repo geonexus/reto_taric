38f583830e50bf26bdeff2eaa479cc936eebe9b8
Guillermo Jimenez Prieto <geonexus@gmail.com>
2016-01-31 18:02:32 +0100

    adding search_type to search form

--- taric_books/forms.py
+++ taric_books/forms.py
@@ -1,7 +1,11 @@
 __author__ = 'Geon'
 from django import forms
+from reto_taric.constants import SEARCH_TYPES
 
 class SearchForm(forms.Form):
+
+    search_type = forms.ChoiceField(choices=SEARCH_TYPES,
+                                widget=forms.Select)
     search_value = forms.CharField(label='Insert Text', max_length=100)
 
 class PageForm(forms.Form):
