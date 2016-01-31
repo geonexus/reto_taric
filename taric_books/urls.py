a09b1b79644d03bf045bb4af69a60f560ccfa273
Guillermo Jimenez Prieto <geonexus@gmail.com>
2016-01-31 18:01:06 +0100

    refactoring urls

--- taric_books/urls.py
+++ taric_books/urls.py
@@ -6,12 +6,10 @@ from . import views
 urlpatterns = [
     # ex: /taric_books/
     url(r'^$', views.index, name='index'),
-    # ex: /taric_books/results/
-    url(r'^results/$', views.results, name='results'),
-    # ex: /taric_books/author/
-    url(r'^author/$', views.author, name='author'),
-        # ex: /taric_books/author_page/author
-    url(r'^author_page/(?P<author>[-\w]+)/$', views.author_page, name='author_page'),
+    # ex: /taric_books/search/
+    url(r'^search/$', views.search, name='search'),
+        # ex: /taric_books/search_page/search
+    url(r'^search_page/(?P<search>[-\w]+)/(?P<search_type>[-\w]+)/$', views.search_page, name='search_page'),
     # ex: /taric_books/isbn/
     url(r'^(?P<isbn>[-\w]+)/$', views.isbn, name='isbn'),
 
