48780c08f854c6f8d3db21e44baf6e556fc58a4d
Guillermo Jimenez Prieto <geonexus@gmail.com>
2016-02-01 03:34:48 +0100

    refactoring views

--- taric_books/views.py
+++ taric_books/views.py
@@ -2,18 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.text import slugify
 from django.conf import settings
-from .forms import SearchForm, PageForm
+from .forms import Search_form_type, Search_form_value, PageForm
 import isbn_manager
 import gbooks_covers
 
 def index(request):
-    form = SearchForm()
-    return render(request, 'taric_books/index.html', {'form': form})
+    form_type = Search_form_type()
+    form_value = Search_form_value()
+    return render(request, 'taric_books/index.html', {'form_type': form_type, 'form_value': form_value})
 
 def search(request):
     form = PageForm()
     search_value = slugify(request.POST['search_value'])
     search_type = request.POST['search_type']
+
+    if search_value == "":
+        form_type = Search_form_type()
+        form_value = Search_form_value()
+        return render(request, 'taric_books/index.html', {'form_type': form_type, 'form_value': form_value})
+
 
     response = isbn_manager.search_by(search_type, search_value, page=None)
     print response
@@ -57,13 +64,10 @@ def isbn(request, isbn):
     return render(request, 'taric_books/isbn.html', context)
 
 def taric(request):
-    form = SearchForm()
-    return render(request, 'taric_books/taric.html', {'form': form})
+    return render(request, 'taric_books/taric.html')
 
 def github(request):
-    form = SearchForm()
-    return render(request, 'taric_books/github.html', {'form': form})
+    return render(request, 'taric_books/github.html')
 
 def about(request):
-    form = SearchForm()
-    return render(request, 'taric_books/about.html', {'form': form})
+    return render(request, 'taric_books/about.html')
